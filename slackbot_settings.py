2f567be02f9245463d34614b629ecd4282e462cb
Andrew-Takahashi <stalemate_hot_free_net@yahoo.co.jp>
2019-01-21 16:29:20 +0900

    First Commit

---
+++ slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-324922503859-528824871717-HGFhgH7XC1sUQYcSZheDpNnV"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "何言ってだこいつ"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
